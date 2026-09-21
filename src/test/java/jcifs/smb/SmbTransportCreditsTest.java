332089812c71848a21dfb04bb5dc33b6ecb5fa86
Courville Software <software@courville.org>
2026-09-21 11:25:12 +0200

    add authorship

--- src/test/java/jcifs/smb/SmbTransportCreditsTest.java
+++ src/test/java/jcifs/smb/SmbTransportCreditsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026
+ * Copyright (C) Courville Software 2026
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
